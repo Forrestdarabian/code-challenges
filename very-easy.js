1b711def63ee9a4c92a906c5ec24c2e2160f5e76
Forrest-Darabian <mr.smiley72@yahoo.com>
2020-05-07 13:43:05 -0700

    Coding for an hour

--- very-easy.js
+++ very-easy.js
@@ -455,3 +455,20 @@ function getLastItem(arr) {
   const last = arr.pop();
   return last;
 }
+
+// Create a function that returns the string "Burp" with the amount of "r's" determined by the input parameters of the function.
+
+// Examples
+// longBurp(3) ➞ "Burrrp"
+
+// longBurp(5) ➞ "Burrrrrp"
+
+// longBurp(9) ➞ "Burrrrrrrrrp"
+// Notes
+// Expect num to always be >= 1.
+// Remember to use a capital "B".
+
+function longBurp(num) {
+  const letterR = "r".repeat(num);
+  return "Bu" + letterR + "p";
+}
